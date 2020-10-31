388a240c74933c735b721af935d46a6190398644
Vinicius Borges <vineckb@gmail.com>
2020-10-31 04:37:17 -0300

    feat(web): add vuetify

---
+++ web/src/components/ProductItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ProductItem">
+    <img :src="product.image" alt="" />
+    <h3>{{ product.title }}</h3>
+    <p>{{ product.price }}</p>
+    <a :href="`/products/${product.slug}`">details</a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+interface Product {
+  title: String;
+  slug: String;
+  image: String;
+  price: Number;
+}
+
+@Component
+export default class ProductItem extends Vue {
+  product: Product;
+}
+</script>
